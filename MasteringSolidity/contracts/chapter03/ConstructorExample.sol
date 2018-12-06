6f202e638e452f5b6a4397e28a73299c985ff67c
Jitendra Chittoda <jcchittoda@gmail.com>
2018-12-06 08:02:58 +0530

    Add ConstructorExample.sol for constructor example code

---
+++ MasteringSolidity/contracts/chapter03/ConstructorExample.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.24;
+
+contract ConstructorExample {
+
+    string public tokenName;
+    string public symbol;
+    address public owner;
+
+    constructor(string _tokenName, string _symbol) public {
+        owner = msg.sender;
+        tokenName = _tokenName;
+        symbol = _symbol;
+    }
+    //...
+}
+
+contract NoConstructor {
+    string public tokenName = "Sample Token";
+    string public symbol = "SYMB";
+    address public owner = msg.sender;
+}
