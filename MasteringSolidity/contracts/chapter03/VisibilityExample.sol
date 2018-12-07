f39f52362bd4b9c150ad7e6b9b6486e8a184dff7
Jitendra Chittoda <jcchittoda@gmail.com>
2018-12-07 08:09:23 +0530

    Add VisibilityExample.sol to show variable and function visibility.

---
+++ MasteringSolidity/contracts/chapter03/VisibilityExample.sol
@@ -0,0 +1,41 @@
+pragma solidity ^0.4.24;
+
+contract SuperContract {
+    uint internal data;
+
+    function multiply(uint _a) private pure returns (uint) { return _a * 2; }
+    function setData(uint _a) internal { data = _a; }
+    function externalFn() external returns (uint) { /*...*/ }
+    function publicFn() public returns (uint) { /*...*/ }
+}
+
+contract VisibilityExample is SuperContract {
+
+    function readData() public {
+        //Following calls: error: not accessible
+        //uint result = multiply(2);
+        //externalFn();
+
+        //Following calls: Allowed access
+        data = data * 5; //variable accessible
+        setData(10); //function accessible
+        this.externalFn();
+        publicFn();
+    }
+}
+
+//Contract accessing VisibilityExample contract
+contract ExternalContract {
+    VisibilityExample ve = VisibilityExample(0x1);
+
+    function accessOtherContract() public {
+        //Following calls: error: not accessible
+        //ve.setData(10);
+        //ve.multiply(10);
+
+        //Following calls: Allowed access
+        ve.externalFn();
+        ve.publicFn();
+        ve.readData();
+    }
+}
