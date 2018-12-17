c47f4c5aa26b1019af756a48ab3577652c1d256c
Jitendra Chittoda <jcchittoda@gmail.com>
2018-12-17 10:43:44 +0530

    Add EventExample.sol to show Solidity events.

---
+++ MasteringSolidity/contracts/chapter03/EventExample.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.24;
+
+contract EventExample {
+    uint public balance;
+    event Deposited(address indexed from, uint amount);
+
+    function () external payable {
+        balance += msg.value;
+        emit Deposited(msg.sender, msg.value);
+    }
+}
