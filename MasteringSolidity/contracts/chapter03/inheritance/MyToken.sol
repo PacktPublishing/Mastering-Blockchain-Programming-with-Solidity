6c6c9b8eadb2a39a855dd46bbfa833abd312211d
Jitendra Chittoda <jcchittoda@gmail.com>
2018-12-18 07:29:04 +0530

    Add MyToken.sol to show example of contract argument passing in inheritance

---
+++ MasteringSolidity/contracts/chapter03/inheritance/MyToken.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.4.24;
+
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+
+contract DetailedERC20 is ERC20 {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+
+    constructor(string _name, string _symbol, uint8 _decimals ) public {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+    }
+}
+
+contract MyToken is DetailedERC20("MyToken", "MTKN", 18) {
+}
+
+contract MyToken2 is DetailedERC20 {
+    constructor (
+        string _name,
+        string _symbol,
+        uint8 _decimals
+    ) DetailedERC20(_name, _symbol, _decimals) public {
+
+    }
+}
