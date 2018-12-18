db32a61e4b3bc632e5c1ce67cde3e5f29880b31f
Jitendra Chittoda <jcchittoda@gmail.com>
2018-12-19 05:06:41 +0530

    Add ExampleInterface.sol to show interface

---
+++ MasteringSolidity/contracts/chapter03/ExampleInterface.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.4.24;
+
+interface ExampleInterface {
+    function transfer(address _to, uint _amount) external returns (bool);
+}
+
+/*
+pragma solidity 0.5.0;
+
+interface ExampleInterface {
+    enum Status {Pending, Inprocess, Processed}
+    struct Data {
+        address requester;
+        uint amount;
+        Status status;
+    }
+    function transfer(address _to, uint _amount) external;
+}
+*/
+
