dc18fec609cb5e42a1cf4176fb014194fc75822f
Jitendra Chittoda <jcchittoda@gmail.com>
2018-12-18 05:38:19 +0530

    Add Inheritance Examples

---
+++ MasteringSolidity/contracts/chapter03/InheritanceExample2.sol
@@ -0,0 +1,44 @@
+pragma solidity ^0.4.24;
+
+contract Ownable {
+    address public owner;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    constructor() public {
+        owner = msg.sender;
+    }
+}
+
+contract ValueStorage is Ownable {
+    uint public value = 2;
+
+    function update() public onlyOwner {
+        value += 1;
+    }
+
+    //...
+}
+
+contract ValueStorage1 is ValueStorage {
+
+    function update() public {
+        value *= 2;
+        super.update();
+    }
+}
+
+contract ValueStorage2 is ValueStorage {
+
+    function update() public {
+        value *= 3;
+        super.update();
+    }
+}
+
+contract InheritanceExample2 is ValueStorage1, ValueStorage2 {
+
+}
