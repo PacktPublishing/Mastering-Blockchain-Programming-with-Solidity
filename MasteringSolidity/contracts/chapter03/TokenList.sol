03a732bb9b1802d71f3a69ae33feda4920979a39
Jitendra Chittoda <jcchittoda@gmail.com>
2018-12-19 11:24:16 +0530

    Add library examples

---
+++ MasteringSolidity/contracts/chapter03/TokenList.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.4.24;
+
+import "./ControlledAddressList.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+contract TokenList is Ownable {
+
+    using ControlledAddressList for ControlledAddressList.Data;
+    ControlledAddressList.Data data;
+
+    function add(address _token) public onlyOwner returns (bool) {
+        return data.enable(_token);
+    }
+
+    function remove(address _token) public onlyOwner returns (bool) {
+        return data.disable(_token);
+    }
+
+    function isPresent(address _token) public view returns (bool) {
+        return data.isEnabled(_token);
+    }
+}
