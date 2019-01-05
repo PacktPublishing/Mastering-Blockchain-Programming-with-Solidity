69d519c2f26fd50e7a1497d2b8f19e052e6e83f9
Jitendra Chittoda <jcchittoda@gmail.com>
2019-01-05 12:43:02 +0530

    Add DeploymentExample.sol sample code to show deployment process.

---
+++ MasteringSolidity/contracts/chapter04/DeploymentExample.sol
@@ -0,0 +1,54 @@
+pragma solidity 0.4.25;
+
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+
+
+/**
+ * @title Contract Deployment Example
+ */
+contract DeploymentExample is Ownable {
+    using SafeMath for uint;
+
+    mapping(address => uint) public balances;
+    uint public totalDeposited;
+
+    event Deposited(address indexed who, uint amount);
+    event Withdrawn(address indexed who, uint amount);
+
+    /**
+     * @dev The fallback function to receive ether.
+     */
+    function() external payable {
+        depositEther();
+    }
+
+    /**
+     * @dev Deposits Ether to the contract
+     */
+    function depositEther() public payable {
+        require(msg.value > 0);
+        balances[msg.sender] = balances[msg.sender].add(msg.value);
+        totalDeposited = totalDeposited.add(msg.value);
+        emit Deposited(msg.sender, msg.value);
+    }
+
+    /**
+     * @dev Withdraw the deposit ether balance.
+     * @param _amount Amount to withdraw.
+     */
+    function withdraw(uint _amount) public {
+        require(balances[msg.sender] >= _amount);
+        balances[msg.sender] = balances[msg.sender].sub(_amount);
+        totalDeposited = totalDeposited.sub(_amount);
+        msg.sender.transfer(_amount);
+        emit Withdrawn(msg.sender, _amount);
+    }
+
+    /**
+     * @dev Destroy the contract and send all ether balance to owner.
+     */
+    function kill() public onlyOwner {
+        selfdestruct(owner);
+    }
+}
