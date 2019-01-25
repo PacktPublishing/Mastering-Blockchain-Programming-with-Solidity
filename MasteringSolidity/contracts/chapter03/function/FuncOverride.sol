24b85a2cae325889bc0382e8a66f3fb1fd23ebfa
Jitendra Chittoda <jcchittoda@gmail.com>
2019-01-25 10:45:07 +0530

    Fixed OpenZeppelin usage in FuncOverride.sol

--- MasteringSolidity/contracts/chapter03/function/FuncOverride.sol
+++ MasteringSolidity/contracts/chapter03/function/FuncOverride.sol
@@ -1,10 +1,10 @@
 pragma solidity 0.4.25;
 
-import "openzeppelin-solidity/contracts/token/ERC20/BasicToken.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-solidity/contracts/lifecycle/Pausable.sol";
 
 
-contract FuncOverride is BasicToken, Pausable {
+contract FuncOverride is ERC20, Pausable {
 
     //Overriding transfer function of BasicToken.sol
     function transfer(address to, uint256 value)
