b3c4267e4a841522a7cde619cf8a6d91d59e6497
Jitendra Chittoda <jitendra@chainsecurity.com>
2019-01-29 07:02:13 +0530

    Add Claimable.sol

---
+++ MasteringSolidity/contracts/chapter09/ownership/Claimable.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.5.0;
+
+import "./Ownable.sol";
+
+
+/**
+ * @title Claimable
+ * @dev Extension for the Ownable contract, where the ownership needs to be claimed.
+ * This allows the new owner to accept the transfer.
+ */
+contract Claimable is Ownable {
+  address public pendingOwner;
+
+  /**
+   * @dev Modifier throws if called by any account other than the pendingOwner.
+   */
+  modifier onlyPendingOwner() {
+    require(msg.sender == pendingOwner);
+    _;
+  }
+
+  /**
+   * @dev Allows the current owner to set the pendingOwner address.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) public onlyOwner {
+    pendingOwner = newOwner;
+  }
+
+  /**
+   * @dev Allows the pendingOwner address to finalize the transfer.
+   */
+  function claimOwnership() public onlyPendingOwner {
+    _transferOwnership(pendingOwner);
+    pendingOwner = address(0);
+  }
+}
