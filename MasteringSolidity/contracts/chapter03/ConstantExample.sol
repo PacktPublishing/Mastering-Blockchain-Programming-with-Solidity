f9da86cacea2a8f22d5da3970d76081765ffd4d4
Jitendra Chittoda <jcchittoda@gmail.com>
2019-03-03 00:58:00 +0530

    Removed chapter03 from MasteringSolidity as moved to Chapter03

--- MasteringSolidity/contracts/chapter03/ConstantExample.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity 0.5.3;
-
-
-contract ConstantExample {
-    string public constant symbol = "TKN";
-    uint public constant totalSupply = 10 ** 9;
-    bytes32 public constant hash = keccak256(abi.encodePacked(symbol));
-}
