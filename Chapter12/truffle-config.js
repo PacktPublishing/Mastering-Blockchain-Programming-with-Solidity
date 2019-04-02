32b03d72ec1887d1365713f44d107a150bbb3abc
Jitendra Chittoda <jcchittoda@gmail.com>
2019-04-03 05:07:58 +0530

    Update config and packages.

--- Chapter12/truffle-config.js
+++ Chapter12/truffle-config.js
@@ -18,11 +18,11 @@
  *
  */
 
-const HDWalletProvider = require('truffle-hdwallet-provider');
-const infuraKey = "fj4jll3k.....";
+ const HDWalletProvider = require('truffle-hdwallet-provider');
+ const infuraKey = "ab8920cd88cd41c2bc71477fdedccb06";
 
-const fs = require('fs');
-const mnemonic = fs.readFileSync(".secret").toString().trim();
+ const fs = require('fs');
+ const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   /**
@@ -60,14 +60,23 @@ module.exports = {
 
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/${infuraKey}`),
-      // network_id: 3,       // Ropsten's id
-      // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+     rinkeby: {
+       provider: () => new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/v3/${infuraKey}', 0, 4),
+       network_id: 4,       // rinkeby's id
+       gas: 6000000,
+       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+     },
+
+     //mainnet: {
+       //provider: () => new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/${infuraKey}', 0, 4),
+       //network_id: 1,
+       //gas: 8000000,
+       //confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+       //timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+//       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+     //},
 
     // Useful for private networks
     // private: {
@@ -85,8 +94,8 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-       version: "0.5.7",    // Fetch exact version from solc-bin (default: truffle's version)
-       docker: false,        // Use "0.5.1" you've installed locally with docker (default: false)
+       version: "^0.5.0",    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
        settings: {          // See the solidity docs for advice about optimization and evmVersion
         optimizer: {
           enabled: true,
