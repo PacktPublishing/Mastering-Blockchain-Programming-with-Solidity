02247ed59e837c56162364b2f7e60ab033239df7
Jitendra Chittoda <jcchittoda@gmail.com>
2019-04-03 05:08:18 +0530

    Fixed migration script

--- Chapter12/migrations/2_deployCrowdsale.js
+++ Chapter12/migrations/2_deployCrowdsale.js
@@ -1,12 +1,12 @@
 const MSTToken = artifacts.require("MSTToken");
 const MSTCrowdsale = artifacts.require("MSTCrowdsale");
 
-module.exports = function(deployer, accounts) {
+module.exports = function(deployer, network, accounts) {
   var owner = accounts[0];
   var wallet = accounts[1];
 
   deployer.deploy(MSTToken, "Mastering Solidity Token", "MST", 18);
 
   var startTime = 1554076800; // 01 Apr 2019 00:00:00 GMT
-  deployer.deploy(MSTCrowdsale, startTime, wallet, MSTToken.address)
+  //deployer.deploy(MSTCrowdsale, startTime, wallet, MSTToken.address)
 };
