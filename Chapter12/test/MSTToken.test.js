fccfb45c5680acf55a5d04cd31845836a0c9d0ca
Jitendra Chittoda <jcchittoda@gmail.com>
2019-04-03 12:13:37 +0530

    Add more test cases

--- Chapter12/test/MSTToken.test.js
+++ Chapter12/test/MSTToken.test.js
@@ -1,3 +1,5 @@
+const { expectEvent, shouldFail } = require('openzeppelin-test-helpers');
+
 const MSTToken = artifacts.require('MSTToken');
 
 let mstToken;
@@ -44,28 +46,77 @@ contract('MSTToken', function (accounts) {
   });
 
   describe('as ERC20', async function () {
-    it('should return totalSupply', async function () {
+    beforeEach(async function() {
+      mstToken = await MSTToken.new("MSTTest Token", "MST", 18);
+      await mstToken.mint(user1, 1000);
+    });
 
+    it('should return totalSupply', async function () {
+        assert(await mstToken.totalSupply(), 1000);
     });
 
     it('should return balanceOf', async function () {
-
+        assert(await mstToken.balanceOf(owner), 0);
+        assert(await mstToken.balanceOf(user1), 1000);
+        assert(await mstToken.balanceOf(user2), 0);
+        assert(await mstToken.balanceOf(other), 0);
     });
 
     it('should transfer', async function () {
+        await mstToken.transfer(user2, 500, {from: user1});
+        assert(await mstToken.balanceOf(user1), 500);
+        assert(await mstToken.balanceOf(user2), 500);
+    });
 
+    it('should fail transfer', async function () {
+        await shouldFail.reverting(
+            mstToken.transfer(user2, 500, {from: owner})
+        );
+        assert(await mstToken.balanceOf(owner), 0);
+        assert(await mstToken.balanceOf(user1), 1000);
+        assert(await mstToken.balanceOf(user2), 0);
+        assert(await mstToken.balanceOf(other), 0);
     });
 
     it('should approve', async function () {
+        assert(await mstToken.allowance(user1, user2), 0);
 
+        await mstToken.approve(user2, 500, {from: user1});
+
+        assert(await mstToken.balanceOf(user1), 1000);
+        assert(await mstToken.balanceOf(user2), 0);
+        assert(await mstToken.allowance(user1, user2), 500);
     });
 
     it('should return allowance', async function () {
+        assert(await mstToken.allowance(user1, user2), 0);
 
+        await mstToken.approve(user2, 1000, {from: user1});
+
+        assert(await mstToken.balanceOf(user1), 1000);
+        assert(await mstToken.balanceOf(user2), 0);
+        assert(await mstToken.allowance(user1, user2), 1000);
     });
 
     it('should transferFrom', async function () {
+        assert(await mstToken.balanceOf(user1), 1000);
+        assert(await mstToken.balanceOf(user2), 0);
+        assert(await mstToken.balanceOf(other), 0);
+        assert(await mstToken.allowance(user1, user2), 0);
 
+        await mstToken.approve(user2, 500, {from: user1});
+
+        assert(await mstToken.balanceOf(user1), 1000);
+        assert(await mstToken.balanceOf(user2), 0);
+        assert(await mstToken.balanceOf(other), 0);
+        assert(await mstToken.allowance(user1, user2), 500);
+
+        await mstToken.transferFrom(user1, other, 250, {from: user2});
+
+        assert(await mstToken.balanceOf(user1), 750);
+        assert(await mstToken.balanceOf(user2), 0);
+        assert(await mstToken.balanceOf(other), 250);
+        assert(await mstToken.allowance(user1, user2), 250);
     });
 
     it('should increaseAllowance', async function () {
