e666d8d82c2f8f6a7e802496f56c09019f03cb2f
Jitendra Chittoda <jcchittoda@gmail.com>
2019-04-04 11:50:03 +0530

    Update config

--- Chapter12/truffle-config.js
+++ Chapter12/truffle-config.js
@@ -95,13 +95,13 @@ module.exports = {
   compilers: {
     solc: {
        version: "^0.5.0",    // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+       docker: false,      // Use "0.5.1" you've installed locally with docker (default: false)
        settings: {          // See the solidity docs for advice about optimization and evmVersion
         optimizer: {
           enabled: true,
           runs: 200
         },
-      //  evmVersion: "byzantium"
+        evmVersion: "byzantium"
        }
     }
   }
