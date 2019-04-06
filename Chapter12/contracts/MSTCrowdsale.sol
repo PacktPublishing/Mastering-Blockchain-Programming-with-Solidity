6c2417e6ef245a1d4bcf440ed7641ef105b6f3cb
Jitendra Chittoda <jcchittoda@gmail.com>
2019-04-06 10:45:13 +0530

    Added comments in the code.

--- Chapter12/contracts/MSTCrowdsale.sol
+++ Chapter12/contracts/MSTCrowdsale.sol
@@ -6,10 +6,26 @@ import "openzeppelin-solidity/contracts/crowdsale/emission/MintedCrowdsale.sol";
 import "openzeppelin-solidity/contracts/crowdsale/validation/CappedCrowdsale.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 
-
+/**
+ * @title Mastering Solidity Token Crowdsale contract.
+ * @dev Contract accept ETH and mint new MSTToken for the ETH sender.
+ * The Crowdsale has some features:
+ * CappedCrowdsale: Max ETH cap the Crowdsale accept.
+ * TimedCrowdsale: Crowdsale starts on openingTime and closes on closingTime.
+ * MintedCrowdsale: The Crowdsale mints new token via the MSTToken contract.
+ */
 contract MSTCrowdsale is
     CappedCrowdsale, TimedCrowdsale, MintedCrowdsale {
 
+    /**
+     * @dev Contract constructor accepts parameter and initialize the crowdsale.
+     * @param _rate Rate for the Crowdsale contract.
+     * @param _wallet Wallet address where ETH will be sent.
+     * @param _token MSTToken address.
+     * @param _openingTime Opening time of the Crowdsale.
+     * @param _closingTime Closing time of the Crowdsale.
+     * @param _cap Max cap in ETH for Crowdsale.
+     */
     constructor(
         uint256 _rate,
         address payable _wallet,
--- Chapter12/contracts/MSTToken.sol
+++ Chapter12/contracts/MSTToken.sol
@@ -6,9 +6,24 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
 
+/**
+ * @title Mastering Solidity Token Contract
+ * An ERC20 token which has the following features:
+ * ERC20Burnable
+ * ERC20Pausable
+ * ERC20Mintable
+ * ERC20Detailed
+ */
 contract MSTToken is
     ERC20Burnable, ERC20Pausable, ERC20Mintable, ERC20Detailed {
 
+    /**
+     * @dev Contract constructor accepts parameter and initialize the
+     * token contract.
+     * @param _name Name of the token.
+     * @param _symbol Symbol of the token.
+     * @param _decimals Decimals of the token.
+     */
     constructor(
         string memory _name,
         string memory _symbol,
