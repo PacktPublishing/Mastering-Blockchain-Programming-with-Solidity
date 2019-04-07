05ac24334dfcdf5e2bd3961e91b421676179be35
Jitendra Chittoda <jcchittoda@gmail.com>
2019-04-07 09:07:16 +0530

    Update config.

--- Chapter12/truffle-config.js
+++ Chapter12/truffle-config.js
@@ -61,16 +61,17 @@ module.exports = {
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
      rinkeby: {
-       provider: () => new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/v3/${infuraKey}', 0, 4),
+       provider: () => new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/${infuraKey}', 0, 2),
        network_id: 4,       // rinkeby's id
        gas: 6000000,
+       gasPrice: 10000000000,
        confirmations: 2,    // # of confs to wait between deployments. (default: 0)
        timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
        skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
      },
 
      //mainnet: {
-       //provider: () => new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/${infuraKey}', 0, 4),
+       //provider: () => new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/${infuraKey}', 0, 2),
        //network_id: 1,
        //gas: 8000000,
        //confirmations: 2,    // # of confs to wait between deployments. (default: 0)
